202361eb274e4f471fc426a2300e09abb361c04d
Joey <joey.alzamora@gmail.com>
2020-04-24 22:53:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,19 +27,23 @@ def line(katz_deli)
   end
   puts "The line is currently: " << new_deli.join(" ")
 end
-  
-
 
 end
-    
-    
-
-def take_a_number(katz_deli, new_customer)
   
 
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 
 end
 
-
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift()
+  end
+end
 
 
